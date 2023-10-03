298f48beb02286271e05522369951ec428f03238
Phillip Lei <plgrx@umsystem.edu>
2023-10-03 17:49:52 +0200

    Server.js adjustments

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const axios = require('axios');
-
 const express = require("express");
 const stockfish = require("stockfish");
 const engine = stockfish();
 const app = express();
+var http = require('http');
 const port = process.env.PORT || 5000;
 
 const fenregex = "/^([rnbqkpRNBQKP1-8]+\/){7}([rnbqkpRNBQKP1-8]+)\s[bw]\s(-|K?Q?k?q?)\s(-|[a-h][36])\s(0|[1-9][0-9]*)\s([1-9][0-9]*)/"
+
+// app.get('/', (req, res) => {
+//   res.send('Its Chess World!')
+// })
 
 engine.onmessage = function(msg) {
   console.log(msg);
@@ -17,12 +21,8 @@ engine.postMessage("uci");
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-app.post('/', (request, response) => {
-
-  // if (!request.body.fen.match(fenregex)) {
-  //   response.send("Invalid fen string");
-  //   return;
-  // }
+// Receive request to calculate a best move
+app.post('/bestmove', (request, response) => {
   
 // if chess engine replies
   engine.onmessage = function(msg) {
@@ -36,29 +36,16 @@ app.post('/', (request, response) => {
         response.send(msg);
     }
   }
-
+  
 // run chess engine
   engine.postMessage("ucinewgame");
   engine.postMessage("position fen " + request.body.fen);
   engine.postMessage("go depth 18");
 });
 
-// // Define a ping/health-check route
-// app.get('/api', (req, res) => {
-//   res.json({ message: 'Connection to the backend is working!' });
-// });
-
-// FEN TEST
-axios.post('http://localhost:5000', {
-  fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1'
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('Something bad happened', err);
+  }
+  console.log('Server is listening on', port);
 })
-.then(response => {
-  console.log(response.data);
-})
-.catch(error => {
-  console.error(error);
-});
-
-app.listen(port, () => {
-  console.log("Server is listening on port " + port);
-});
