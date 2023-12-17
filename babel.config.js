f1e2434eef9d21a11cc391eba829ab1ce6cec937
CandymanAndy <74220837+AndrewChang-cpu@users.noreply.github.com>
2023-12-17 14:39:29 -0600

    Bug fix: preset in babel config causing problems with native dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['@babel/preset-env', 'babel-preset-expo', '@babel/preset-typescript', 'module:metro-react-native-babel-preset'],
+    presets: ['babel-preset-expo', '@babel/preset-typescript', 'module:metro-react-native-babel-preset'],
     plugins: ['react-native-reanimated/plugin']
     // ["@babel/plugin-transform-class-properties", { "loose": false }],
     // ["@babel/plugin-transform-private-methods", { "loose": false }],
